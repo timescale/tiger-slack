6e44fb1f11579a17610f9049df2d0725863c261f
Greg Saab <greg@timescale.com>
2025-09-10 10:49:00 -0400

    ruff

--- ingest/tiger_slack/utils.py
+++ ingest/tiger_slack/utils.py
@@ -9,7 +9,9 @@ from psycopg_pool import AsyncConnectionPool
 async def is_table_empty(pool: AsyncConnectionPool, table_name: str) -> bool:
     async with pool.connection() as con, con.cursor() as cur:
         await cur.execute(
-            sql.SQL("SELECT EXISTS(SELECT 1 FROM slack.{} LIMIT 1)").format(sql.Identifier(table_name))
+            sql.SQL("SELECT EXISTS(SELECT 1 FROM slack.{} LIMIT 1)").format(
+                sql.Identifier(table_name)
+            )
         )
         row = await cur.fetchone()
         if not row:
