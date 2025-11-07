f6c28e0a210cc4c358a7c3ca7b696d09ae0929c8
Greg Saab <greg@timescale.com>
2025-11-07 11:01:27 -0500

    needs to be a string

--- mcp/src/util/messageFields.ts
+++ mcp/src/util/messageFields.ts
@@ -1,8 +1,9 @@
-export const getMessageFields = (includeFiles?: boolean): string[] => [
-  'ts::text',
-  'channel_id',
-  'text',
-  'm.user_id',
-  'thread_ts::text',
-  ...(includeFiles ? ['files::jsonb'] : []),
-];
+export const getMessageFields = (includeFiles?: boolean): string =>
+  [
+    'ts::text',
+    'channel_id',
+    'text',
+    'm.user_id',
+    'thread_ts::text',
+    ...(includeFiles ? ['files::jsonb'] : []),
+  ].join(',');
