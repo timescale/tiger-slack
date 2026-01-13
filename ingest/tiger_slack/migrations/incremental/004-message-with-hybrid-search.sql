a05b589f8b51bc7e40d03c36d806ea336f883ad8
Greg Saab <greg@timescale.com>
2026-01-13 13:05:20 -0500

    need to set the search paths

--- ingest/tiger_slack/migrations/incremental/004-message-with-hybrid-search.sql
+++ ingest/tiger_slack/migrations/incremental/004-message-with-hybrid-search.sql
@@ -6,11 +6,12 @@
 create extension if not exists vectorscale CASCADE;
 create extension if not exists pg_textsearch;
 
+set local search_path to public, timescale_functions, pg_catalog;
 
 -- pg_textsearch now supports hypertables, but it does not support compression/chunking
 -- so we need to disable it and decompress all chunks
-select remove_compression_policy('slack.message');
-select decompress_chunk(c, true) from show_chunks('slack.message', CURRENT_TIMESTAMP, '2000-1-1') c;
+perform remove_compression_policy('slack.message');
+perform decompress_chunk(c, true) from show_chunks('slack.message', CURRENT_TIMESTAMP, '2000-1-1') c;
 alter table slack.message set (timescaledb.compress=false);
 
 -- add column for embeddings
