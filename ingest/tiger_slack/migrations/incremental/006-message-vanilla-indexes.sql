6cc06eb5d5c683da2e96fe693fd599cd1a252988
Greg Saab <greg@timescale.com>
2026-01-24 00:15:25 -0500

    [AGE-305] Switch to diskann for vector index (#93)
    ## Why
    Was very costly to create the vector index with hnsw.
    
    <img width="1353" height="333" alt="image"
    src="https://github.com/user-attachments/assets/6e21ee6e-b6c1-405f-a1fe-46e00dd81024"
    />

--- ingest/tiger_slack/migrations/incremental/006-message-vanilla-indexes.sql
+++ ingest/tiger_slack/migrations/incremental/006-message-vanilla-indexes.sql
@@ -2,10 +2,12 @@
 -- adding indexes onto the new table
 -- adding these separately so that migration/backfill was more performant
 
+create extension if not exists vectorscale CASCADE;
+
 create index on slack.message_vanilla (channel_id, thread_ts, ts asc) where thread_ts is not null;
 create index on slack.message_vanilla (channel_id, thread_ts, ts desc) where thread_ts is not null;
 create index on slack.message_vanilla (user_id, thread_ts, channel_id) where thread_ts is not null;
 
 -- create bm25 and vector indexes
 create index message_vanilla_text_bm25_idx on slack.message_vanilla using bm25(text) with (text_config='english');
-create index message_vanilla_text_vector_idx on slack.message_vanilla using hnsw(embedding vector_cosine_ops) with (m = 20, ef_construction = 64);
+create index message_vanilla_text_vector_idx on slack.message_vanilla using diskann (embedding vector_cosine_ops);
